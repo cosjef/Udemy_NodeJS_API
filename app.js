684e5dfa8221841e093a0c704d8638faf4bf742f
Jeffrey Costa <cosjef@gmail.com>
2016-11-05 18:57:39 -0400

    After completing section 2

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 var restify = require('restify');
 var server = restify.createServer();
+var morgan = require('morgan');
 
 // array of users
 var users = {};
 var max_user_id = 0;
 
+// use morgan to log requests to the console
+server.use(morgan('dev'));
 
 server.use(restify.acceptParser(server.acceptable));
 server.use(restify.bodyParser());
+server.use(restify.queryParser());
 
 
 // If the root URL is requested
@@ -24,6 +28,40 @@ server.get("/", function(req, res, next) {
 	res.end(JSON.stringify(users));
 	// go to the next part of the execution
 	// no further execution takes place after this function
+	return next();
+})
+
+
+// Get a specific user ID
+server.get("/user/:id", function(req, res, next) {
+	res.setHeader('content-type', 'application/json');
+	res.writeHead(200);
+	res.end(JSON.stringify(users[parseInt(req.params.id)]));
+	return next();
+});
+
+
+// Put endpoint to update a record
+server.put("/user/:id", function(req, res, next) {
+	var user = users[parseInt(req.params.id)];
+	// store the body of the put update in updates variable
+	var updates = req.params;
+	// Loop through whatever came in via the updates variable - an array of params
+	for (var field in updates) { 
+		user[field] = updates[field];
+	}
+	res.setHeader('content-type', 'application/json');
+	res.writeHead(200);
+	res.end(JSON.stringify(user));
+	return next();
+})
+
+// DELETE a user
+server.del("user/:id", function(req, res, next) {
+	delete users[parseInt(req.params.id)];
+	res.setHeader('content-type', 'application/json');
+	res.writeHead(200);
+	res.end(JSON.stringify(true));
 	return next();
 })
 
