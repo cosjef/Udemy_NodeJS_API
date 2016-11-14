04770d18aafe27f6fb2a9e080a54af21fc6a812d
Jeffrey Costa <cosjef@gmail.com>
2016-11-13 21:39:48 -0500

    After lesson 25

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
+// Require these packages in your app
 var restify = require('restify');
 var server = restify.createServer();
 var morgan = require('morgan');
 var setupController = require('./controllers/setupController.js');
 var userController = require('./controllers/userController.js');
 var restifyValidator = require('restify-validator');
+var config = require('./config/dbConnections.js');
+var mongoose = require('mongoose');
+
+
+mongoose.connect(config.getMongoConnection());
 
 
 setupController(server, restify, restifyValidator);
---
+++ config/dbConnections.js
@@ -0,0 +1,7 @@
+// mwke what is in here public
+module.exports = {
+	getMongoConnection: function() {
+		return 'mongodb://testuser:testpass@ds151917.mlab.com:51917/nodesampleapi';
+	}
+
+}
